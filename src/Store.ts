86d8565632a813e158d8f0b06a5669f84a931cdd
jicking <jicking.dev@gmail.com>
2020-09-23 00:45:43 +0800

    configure redux persist

--- src/Store.ts
+++ src/Store.ts
@@ -1,7 +1,33 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER
+} from 'redux-persist'
 import { rootReducer } from './Reducer';
+import storage from 'redux-persist/lib/storage'
+
+const STORAGE_KEY = 'rtk00'
+
+const persistConfig = {
+	key: STORAGE_KEY,
+	storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: rootReducer,
-	middleware: getDefaultMiddleware()
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware({
+		serializableCheck: {
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+		}
+	})
 });
+
+export const persistor = persistStore(store);
